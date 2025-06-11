7e57b489c229198d8b73a0dd82b913d66057a8c4
Isaac Hatton <IsaacHatton@outlook.com>
2025-06-11 14:21:54 +0000

    remove clarity

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { PdfEditor } from './PdfEditor';
 import { Pdf, PdfPicker } from './PdfPicker';
 import './pdf-editor.css';
 
-import Clarity from '@microsoft/clarity';
-Clarity.init('rxebkxz5mx');
-
 if (typeof (Promise as any).withResolvers === 'undefined') {
   (Promise as any).withResolvers = function <T>() {
     let resolve: (value: T | PromiseLike<T>) => void;
